94e6bb71ec7bef0b40eef5b8eb8b83f95e797e9a
Kota Nonaka <kota.kaicho@gmail.com>
2018-01-28 00:13:58 +0900

    Modifi seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,17 @@ Article.create(
   body: 'これはテスト記事です。ほげほげ〜〜',
   user: user
 )
+
+project = Project.create!(
+  title: 'Cloud Service Center',
+  description: '本学の全学生が利用するサービスを提供しているCloud Service Centerに関する研究・開発を行うプロジェクトです。',
+  feed_url: 'https://example.com/feed.xml',
+  url: 'https://example.com/',
+  icon_url: 'https://example.com/icon.png'
+)
+
+ProjectArticle.create!(
+  title: 'CSCでサービスつくりました',
+  published_at: Time.gm(2018, 1, 27),
+  project: project
+)
