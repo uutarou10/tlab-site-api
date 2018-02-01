c7d1ea271ca45cb1d20d7c30cad760ad8c34516d
Kota Nonaka <kota.kaicho@gmail.com>
2018-02-01 23:51:00 +0900

    Add create article method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,18 @@ class ArticlesController < ApplicationController
     article = Article.find(params[:id])
     render json: article
   end
+
+  def create
+    article = Article.new(article_params)
+    article.user = current_user
+    article.save!
+
+    render json: article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   resources :users, only: [:index, :show, :create]
   resources :projects, only: [:index, :create]
-  resources :articles, only: [:index, :show]
+  resources :articles, only: [:index, :show, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
